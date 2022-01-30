448b6b83c011f8271b03659fceed2f6aa552558f
deshwalshalvi <97867919+deshwalshalvi@users.noreply.github.com>
2022-01-30 15:41:54 +0530

    Add files via upload

---
+++ foreachdemo.js
@@ -0,0 +1,34 @@
+import { LightningElement , track} from 'lwc';
+
+
+export default class Iterationdemo extends LightningElement {
+   
+    contacts = [
+        {
+            Id : 'C1',
+            Name : 'Shalvi',
+            Email : 'demo_test@gmail.com'
+        },
+        {
+            Id : 'C2',
+            Name : 'Demo',
+            Email : 'demo@gmail.com'
+        },
+        {
+            Id : 'C3',
+            Name : 'Test',
+            Email : 'test@gmail.com'
+        },
+        {
+            Id : 'C4',
+            Name : 'Test02',
+            Email : 'test02@gmail.com'
+        },
+        {
+            Id : 'C5',
+            Name : 'Test03',
+            Email : 'test03@gmail.com'
+        }
+      ];
+     
+}
